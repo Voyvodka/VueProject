500c5fafe89ad8518178219a107d789b38ce63a8
Voyvodka <smttozkn@gmail.com>
2023-01-01 22:39:41 +0300

    navbar olusturuluyor 05

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@ const menuItems = ref([
 </script>
 <template>
     <nav :class="isAside ? 'left-0 ' : 'top-0'">
-        <ul :class="isAside ? 'inline-block p-30px h-100vh' : 'flex justify-center'">
+        <ul :class="isAside ? 'inline-block p-30px h-100vh' : 'flex justify-center'" class="bg-blue-200">
             <li v-for="menu in menuItems" :key="menu.label" :class="isAside ? 'p-1' : 'p-4'" class="text-xl font-bold">
                 <a :href="menu.link">{{ menu.label }}</a>
             </li>
