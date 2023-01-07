0ba5add1e27e76695608d4706e38fdc73c7572e7
Voyvodka <smttozkn@gmail.com>
2023-01-07 20:07:18 +0300

    deneme

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,24 +11,9 @@ const store = useCounterStore();
     <div>{{ t("language") }}</div>
     <div>{{ t("details.anasayfa") }}</div>
     <div>{{ t("details.insan-kaynaklari") }}</div>
-    <el-button class="gap-5" type="primary" round @click="locale = 'en'">
-        <Icon icon="ic:baseline-remove-red-eye" /> Dili Değiş
-    </el-button>
-    <el-button type="primary" round @click="locale = 'tr'">
-        <Icon icon="ic:baseline-remove-red-eye" /> Dili Değiş
-    </el-button>
     <select class="text-2xl" name="" id="">
         <option value="123">asdf</option>
     </select>
-    <el-row class="mb-4">
-        <el-button round>Default</el-button>
-        <el-button type="primary" round>Primary</el-button>
-        <el-button type="success" round>Success</el-button>
-        <el-button type="info" round>Info</el-button>
-        <el-button type="warning" round>Warning</el-button>
-        <el-button type="danger" round>Danger</el-button>
-    </el-row>
-    <Icon icon="ic:baseline-remove-red-eye" />
 </template>
 <style scoped>
 </style>
--- src/components/MainPageContent.vue
+++ src/components/MainPageContent.vue
@@ -1,65 +1,6 @@
 <script setup lang="ts">
 import { useSettingsStore } from '../stores/userSettings'
 const store = useSettingsStore();
-const { Popover, Toast, Collapse } = bootstrap
-const collapse = Vue.component('bsCollapse', {
-    template: `
-        <div>
-            <slot name="trigger"></slot>
-            <slot name="target"></slot>
-        </div>
-    `,
-    props: {
-        toggle: {
-            required: false,
-            default: false
-        },
-        id: {
-            required: true
-        }
-    },
-    mounted() {
-        var trigger = this.$slots['trigger'][0].elm
-        var target = this.$slots['target'][0].elm
-        target.classList.add('collapse')
-        target.setAttribute('id', this.id);
-        trigger.setAttribute('data-bs-target', '#' + this.id);
-        trigger.setAttribute('data-bs-toggle', 'collapse');
-        new Collapse(target, { toggle: this.toggle })
-    },
-})
-const popover = Vue.component('bsPopover', {
-    template: `
-        <slot/>
-    `,
-    props: {
-        content: {
-            required: false,
-            default: '',
-        },
-        title: {
-            default: 'My Popover',
-        },
-        trigger: {
-            default: 'click',
-        },
-        delay: {
-            default: 0,
-        },
-        html: {
-            default: false,
-        },
-    },
-    mounted() {
-        // pass bootstrap popover options from props
-        var options = this.$props
-        var ele = this.$slots.default[0].elm
-        new Popover(ele, options)
-    },
-})
-new Vue({
-    el: "#app"
-})
 </script>
 <template>
     <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top tag="article"
